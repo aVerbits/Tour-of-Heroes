f861f264022b8a12c7b89ba041df9cc005303b87
Aleksei <alekseiverbitski@gmail.com>
2021-12-20 02:35:53 +0300

    add model with interface Hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../Models/hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit {
+export class HeroesComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+hero: Hero = {
+  id:1,
+  name: "Windshtorm"
+}
 
 }
